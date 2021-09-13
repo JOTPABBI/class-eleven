b8f406c39114842205440a8bef82b5d973f4fdc9
JOTPABBI <86107597+JOTPABBI@users.noreply.github.com>
2021-09-13 23:03:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+function preload(){
+  //pre-load images
+ bombimage=loadImage(bomb.png)
+ coinimage=loadImage(coin.png)
+ energydrinkimage=loadImage(energydrink.png)
+ pathimage=loadImage(path.png)
+ powerimage=loadImage(power.png)
+ 
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+
+}
+
+function draw() {
+  background(0);
+
+}
